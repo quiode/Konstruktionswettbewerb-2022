87a5b5647cf5f85f8d668b6362260f2aef9cf689
Dominik Schwaiger <domi.schwaiger04@gmail.com>
2022-05-01 11:44:57 +0200

    extraced move functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,37 @@
 #include <Arduino.h>
 #include <AFMotor.h>
 
-AF_DCMotor motor1(3);
-AF_DCMotor motor2(4);
+AF_DCMotor motor3(3);
+AF_DCMotor motor4(4);
+
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+
+void maehdrescher(uint8_t speed = 255, uint8_t direction = FORWARD)
+{
+  motor1.setSpeed(speed);
+  motor2.setSpeed(speed);
+
+  motor1.run(direction);
+  motor2.run(direction);
+}
+
+void drive(uint8_t speed = 255, uint8_t direction = FORWARD)
+{
+  motor3.setSpeed(speed);
+  motor4.setSpeed(speed);
+
+  motor3.run(direction);
+  motor4.run(direction);
+}
 
 void setup()
 {
   delay(1000);
 
-  // put your setup code here, to run once:
-  motor1.setSpeed(255);
-  motor2.setSpeed(255);
+  maehdrescher();
 
-  motor1.run(FORWARD);
-  motor2.run(FORWARD);
+  drive();
 }
 
 void loop()
